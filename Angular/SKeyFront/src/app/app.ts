8e7984388f461445f911b2c347ce909b582a47f9
Eng-Khaled_Alqady <khaledalkady779@gmail.com>
2026-07-30 03:35:03 +0300

    Refactor app title and update global styles; remove unused font files

--- Angular/SKeyFront/src/app/app.ts
+++ Angular/SKeyFront/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('SKey ERP');
+  protected readonly title = signal('SKeyFront');
 }
